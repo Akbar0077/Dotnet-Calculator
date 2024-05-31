d4cd83c87ee3d8b32257192206e4c648cd13dc31
Akbar0077 <148612146+Akbar0077@users.noreply.github.com>
2024-05-31 19:08:21 +0530

    Add files via upload
    I am currently doing a pretty simple calculator program in C# and I have most of the program working fine as in the add, subtract, multiply and divide functions work fine

---
+++ CalClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calproj
+{
+    public class CalClass
+    {
+        string oper = "";
+        string firstNumberstr = "";
+        string secondNumberstr = "";
+        public CalClass()
+        {
+
+        }
+        public int Addnumber(int firstNumberstr, int secondNumberstr)
+        {
+            int a = firstNumberstr;
+            int b = secondNumberstr;
+            int result = a + b;
+            return result;
+
+        }
+        public int Subnumber(int firstNumberstr, int secondNumberstr)
+        {
+            int a = firstNumberstr;
+            int b = secondNumberstr;
+            int result = a - b;
+            return result;
+
+        }
+        public int Mulnumber(int firstNumberstr, int secondNumberstr)
+        {
+            int a = firstNumberstr;
+            int b = secondNumberstr;
+            int result = a * b;
+            return result;
+
+        }
+        public int Divnumber(int firstNumberstr, int secondNumberstr)
+        {
+            int a = firstNumberstr;
+            int b = secondNumberstr;
+            int result = a / b;
+            return result;
+
+        }
+        public int Test(params int[]values)
+        {
+            int total = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                total += values[i];
+            }
+            return total;
+
+        }
+}   }
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calproj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string oper = "";
+        string firstNumberstr = "";
+        string secondNumberstr = "";
+        int result = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberClick(object sender, RoutedEventArgs e)
+        {
+
+
+            Button btnnumber = (Button)sender;
+
+            if (oper == "")
+            {
+                firstNumberstr = firstNumberstr + btnnumber.Content.ToString();
+                textresult.Text = firstNumberstr;
+            }
+            else
+            {
+                secondNumberstr = secondNumberstr + btnnumber.Content.ToString();
+                textresult.Text = secondNumberstr;
+            }
+        }
+        private void OperatorClick(object sender, RoutedEventArgs e)
+        {
+            Button btnoperator = (Button)sender;
+            oper = btnoperator.Content.ToString();
+        }
+        private void btnoper_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            //  if (firstNumberstr != "" && secondNumberstr != "" && oper != "")
+            {
+
+                int firstNumber = Convert.ToInt16(firstNumberstr);
+                int secondNumber = Convert.ToInt16(secondNumberstr);
+                int result = 0;
+                CalClass classnum = new CalClass();
+                if (oper == "+")
+                {
+                    result = classnum.Addnumber(firstNumber, secondNumber);
+                }
+                else if (oper == "-")
+                {
+                    result = classnum.Subnumber(firstNumber, secondNumber);
+                }
+                else if (oper == "*")
+                {
+                    result = classnum.Mulnumber(firstNumber, secondNumber);
+                }
+                else if (oper == "/")
+                {
+                    result = classnum.Divnumber(firstNumber, secondNumber);
+                }
+                else if (oper == "+" || oper == "-" || oper == "*" || oper == "/")
+                {
+                    Button btnnumber = (Button)sender;
+                    int btncontent = Convert.ToInt32(btnnumber.Content);
+                    int values = btncontent;
+                    classnum.Test(values, values);
+                }
+                
+                textresult.Text = result.ToString();
+            }
+
+
+        }
+
+
+        private void btnclear_Click(object sender, RoutedEventArgs e)
+        {
+            textresult.Clear();
+            firstNumberstr = "";
+            secondNumberstr = "";
+            oper = "";
+        }
+    }
+}
